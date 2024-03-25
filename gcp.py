7842a9234ecd5d15fbf773e5abc9be3431cc0f52
zero <zero.kwok@foxmail.com>
2024-03-25 18:04:03 +0800

    优化 gcp.py 当遇到错误即终止流程

--- gcp.py
+++ gcp.py
@@ -26,44 +26,53 @@ import os
 import gwalk
 import argparse
 
-def execute(commands:str, onlyShow:bool=False) -> int:
+class ResultError(RuntimeError):  
+    def __init__(self, message, ecode):  
+        super().__init__(message)  
+        self.ecode = ecode  
+
+def execute(commands:str, onlyShow:bool=False):
    gwalk.cprint(commands, 'green')
    if onlyShow:
-      return 0
-   return gwalk.RepoHandler.execute(commands)
+      return
+   code = gwalk.RepoHandler.execute(commands)
+   if code != 0:
+      raise ResultError(f'Run: {commands}', code)
 
 if __name__ == '__main__':
-   parser = argparse.ArgumentParser()
-   parser.add_argument('-a', '--all', action='store_true')
-   parser.add_argument('--show', action='store_true')
-   parser.add_argument('--push', action='store_true')
-   parser.add_argument('commit', nargs='?')
-   args = parser.parse_args()
+   try:
+      parser = argparse.ArgumentParser()
+      parser.add_argument('-a', '--all', action='store_true')
+      parser.add_argument('--show', action='store_true')
+      parser.add_argument('--push', action='store_true')
+      parser.add_argument('commit', nargs='?')
+      args = parser.parse_args()
 
-   if not gwalk.RepoWalk.isRepo(os.getcwd()):
-      gwalk.cprint(f'This is not an valid git repository.', 'red')
-      exit(1)
+      if not gwalk.RepoWalk.isRepo(os.getcwd()):
+         gwalk.cprint(f'This is not an valid git repository.', 'red')
+         exit(1)
 
-   code = 0
-   repo = gwalk.RepoStatus(os.getcwd()).load()
-   branch = repo.repo.active_branch.name
+      repo = gwalk.RepoStatus(os.getcwd()).load()
+      branch = repo.repo.active_branch.name
 
-   if args.push:
-      for r in repo.repo.remotes:
-         code += execute(f'git push {r.name} {branch}', )
-      exit(code)
+      if args.push:
+         for r in repo.repo.remotes:
+            execute(f'git push {r.name} {branch}', args.show)
+         exit(0)
 
-   if repo.match('clean'):
-      gwalk.cprint(f'The git repository is clean.', 'green')
+      if repo.match('clean'):
+         gwalk.cprint(f'The git repository is clean.', 'green')
+         exit(0)
+      execute('git status -s --untracked-files=normal')
+
+      if repo.match('dirty' if args.all else 'modified'):
+         execute('git add -A' if args.all else 'git add -u', args.show)
+         if args.commit:
+            execute(f'git commit -m "{args.commit}"', args.show)
+         else:
+            execute(f'git commit', args.show)
+         for r in repo.repo.remotes:
+            execute(f'git push {r.name} {branch}', args.show)
       exit(0)
-   execute('git status -s --untracked-files=normal')
-
-   if repo.match('dirty' if args.all else 'modified'):
-      code += execute('git add -A' if args.all else 'git add -u', args.show)
-      if args.commit:
-         code += execute(f'git commit -m "{args.commit}"', args.show)
-      else:
-         code += execute(f'git commit', args.show)
-      for r in repo.repo.remotes:
-         code += execute(f'git push {r.name} {branch}', args.show)
-   exit(code)
+   except ResultError as e:
+      exit(e.ecode)
