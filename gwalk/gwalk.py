485b65bec436a6f67713bfca927e533c8d698c8e
zero <zero.kwok@foxmail.com>
2025-03-21 14:54:59 +0800

    更新 版本号: v0.2.1 -> v0.2.2 beta

--- gwalk/gwalk.py
+++ gwalk/gwalk.py
@@ -92,7 +92,7 @@ import traceback
 
 projectName    = 'gwalk'
 projectHome    = 'https://github.com/ZeroKwok/gwalk.git'
-projectVersion = '0.2.1'
+projectVersion = '0.2.2 beta'
 projectAuthor  = 'zero.kwok@foxmail.com'
 
 try:
