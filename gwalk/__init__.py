803802b5c1184a97d3795367d4e8bebbbc8de29c
zero <zero.kwok@foxmail.com>
2025-04-19 22:57:41 +0800

    更新 v0.2.2 beta3 -> 0.2.3 beta0

--- gwalk/__init__.py
+++ gwalk/__init__.py
@@ -2,8 +2,8 @@
 gwalk 是一系列用于管理 Git 仓库的命令行小工具，帮助开发者对大批量的 Git 仓库进行日常维护。
 """
 
-__version__ = "0.2.2"
-__status__  = "beta3"
+__version__ = "0.2.3"
+__status__  = "beta0"
 __author__  = "zero.kwok@foxmail.com"
 
 projectName    = 'gwalk'
